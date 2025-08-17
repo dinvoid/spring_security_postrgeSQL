e84541d6b10aad0dfe59e9e430a056d0d027080e
EldinVoid <dincanoybetalmos@gmail.com>
2025-08-17 15:02:08 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/models/Role.java
@@ -0,0 +1,33 @@
+package com.example.demo.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    public ERole getName() {
+        return name;
+    }
+
+    public void setName(ERole name) {
+        this.name = name;
+    }
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+    public Role() {
+
+    }
+
+    public Role(ERole name) {
+        this.name = name;
+    }
+
+    // getters and setters
+}
---
+++ src/main/java/com/example/demo/payload/response/MessageResponse.java
@@ -0,0 +1,8 @@
+package com.example.demo.payload.response;
+
+public class MessageResponse {
+    String message;
+    public MessageResponse(String message){
+        this.message=message;
+    }
+}
